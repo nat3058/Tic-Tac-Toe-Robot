c34cd2a94966dd73f55556bc7743487260c3cd30
nat3058 <87051052+nat3058@users.noreply.github.com>
2024-01-03 13:21:54 -0500

    added comments to  pixy_driver.c

--- Core/Src/pixy_driver.c
+++ Core/Src/pixy_driver.c
@@ -8,7 +8,7 @@
 #include <pixy_driver.h>
 #include <string.h>
 #include <stdio.h>
-
+// request bytes to obtain the data we need
 uint8_t getBlocks[] = {
 		0xae,	// 16-bit sync
 		0xc1,	// 16-bit syn
@@ -27,25 +27,25 @@ uint8_t getResolution[] = {
 	0xff	// unused
 };
 
-
+// be sure to enable FIFO in IOC UART settings!
 void Pixy_UpdateBoard(UART_HandleTypeDef* phuart, int* board) {
 	uint8_t recvBuf[200];
 
-	HAL_UART_Transmit(phuart, getBlocks, 6, HAL_MAX_DELAY);
+	HAL_UART_Transmit(phuart, getBlocks, 6, HAL_MAX_DELAY); //blocking code
 	HAL_UART_Receive(phuart, recvBuf, 150, 1000);
 
 	int numOfBlocks = recvBuf[3] / 14;
 
 	for (int b = 0; b < numOfBlocks; ++b) {
-		int x_idx = 8 + (14 * b);
+		int x_idx = 8 + (14 * b); // find x idx of the block in our buffer 
 		int y_idx = x_idx + 2;
 
-		uint16_t x = (((uint16_t)(recvBuf[x_idx + 1]) << 8) + recvBuf[x_idx]);
+		uint16_t x = (((uint16_t)(recvBuf[x_idx + 1]) << 8) + recvBuf[x_idx]); //calculate the x_idx
 		uint16_t y = (((uint16_t)(recvBuf[y_idx + 1]) << 8) + recvBuf[y_idx]);
 		printf("block %d coords: (%d, %d)\n\r", b+1, x, y);
 		int idx = Pixy_TranslateCoords(x, y);
 		if (idx >= 0 && idx < 9 && board[idx] == 0) {
-			board[idx] = 1;
+			board[idx] = 1; //mark this human square selected square as taken
 			return;
 		}
 	}
